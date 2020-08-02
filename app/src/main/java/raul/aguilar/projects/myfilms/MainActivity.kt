5f44ffe3b8746748d70dca2974b7c6f17e55d05a
raguilar <r_aguilar11@hotmail.com>
2020-08-01 19:48:48 -0600

    Add navigation bar

--- app/src/main/java/raul/aguilar/projects/myfilms/MainActivity.kt
+++ app/src/main/java/raul/aguilar/projects/myfilms/MainActivity.kt
@@ -2,10 +2,55 @@ package raul.aguilar.projects.myfilms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView:BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        createFragment(PopularFilmsFragment())
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {item ->
+
+            when(item.itemId){
+
+                R.id.popular_tab ->{
+                    val popularFilmsFragment = PopularFilmsFragment()
+                    createFragment(popularFilmsFragment)
+                    true
+                }
+                R.id.search_tab ->{
+                    val searchFilmsFragment = SearchFilmsFragment()
+                    createFragment(searchFilmsFragment)
+                    true
+                }
+                R.id.my_lists_tab ->{
+                    val myListsFilmsFragment = MyListsFilmsFragment()
+                    createFragment(myListsFilmsFragment)
+                    true
+                }
+
+                else -> false
+            }
+
+
+        }
+
+    }
+
+    fun createFragment(fragment:Fragment){
+
+       val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container,fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+
     }
 }
